0346c7130becd836c7b439ddf5a9bb8ccff50162
Abdullah Bin Omar Chowdhury <abdullahbinomarchowdhury@gmail.com>
2025-03-10 18:58:33 +0600

    fix:notice details page not working issue

--- src/pages/noticeDetails.jsx
+++ src/pages/noticeDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { baseUrl } from "../constants/env.constants";
+import PageTitle from "../utils/PageTitle";
 
 const NoticeDetail = () => {
   const { id } = useParams();
