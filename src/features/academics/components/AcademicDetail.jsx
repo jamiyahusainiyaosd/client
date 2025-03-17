d01c9e20bdeac4300325511a17dff379ba261d75
shakibosd <syednazmusshakib94@gmail.com>
2025-03-17 17:58:44 +0600

    feat(academic): academin details fix

--- src/features/academics/components/AcademicDetail.jsx
+++ src/features/academics/components/AcademicDetail.jsx
@@ -1,35 +1,44 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Time from '../../../utils/formateData';
 
-const AcademicDetail = ({ className, classTitle, classSetCount, classStudentCount, classDescription, createdAt, updatedAt, }) => {
+const AcademicDetail = ({
+  className,
+  classTitle,
+  classSetCount,
+  classStudentCount,
+  classDescription,
+  createdAt,
+  updatedAt,
+}) => {
   const navigate = useNavigate();
   const handleBack = () => {
     navigate("/academic");
   };
 
   return (
-    <div className="p-6 sm:p-8 md:p-3 lg:p-12 xl:p-14 border rounded-lg shadow-xl max-w-[700px] w-full bg-transparent">
-      <h1 className="text-2xl font-semibold" style={{color:'wheat'}}>
+    <div className="p-6 sm:p-8 md:p-10 lg:p-12 xl:p-14 border rounded-lg shadow-xl max-w-[700px] w-full bg-transparent">
+      <h1 className="text-2xl sm:text-3xl font-semibold" style={{color:'wheat'}}>
         {className}
       </h1>
-      <hr className="my-4" />
+      <hr className="my-4 border-gray-300" />
 
       <div className="space-y-3">
-        <h2 className="text-xl font-semibold">
+        <h2 className="text-lg sm:text-xl font-semibold">
           {classTitle}
         </h2>
         <p className="text-sm sm:text-base">{classDescription}</p>
         <p className="text-sm sm:text-base mt-2">
-          <span className="font-medium">🪑 আসন সংখ্যা : </span> {classSetCount} |
-          <span className="font-medium"> 👨‍🎓 ছাত্র সংখ্যা : </span>
+          <span className="font-medium">🪑 আসন সংখ্যা:</span> {classSetCount} |
+          <span className="font-medium"> 👨‍🎓 ছাত্র সংখ্যা:</span>{" "}
           {classStudentCount}
         </p>
-        <p className="text-base font-medium">
-        প্রকাশের তারিখ : 🗓️ {Time(createdAt)}
+        <p className="text-sm sm:text-base mt-2">
+          🗓️ <span className="font-medium">এড করার তারিখ:</span>{" "}
+          {new Date(createdAt).toLocaleDateString("bn-BD")}
         </p>
-        <p className="text-base font-mediu">
-        আপডেটের তারিখ : 🗓️ {Time(updatedAt)}
+        <p className="text-sm sm:text-base">
+          🗓️ <span className="font-medium">আপডেট করার তারিখ:</span>{" "}
+          {new Date(updatedAt).toLocaleDateString("bn-BD")}
         </p>
       </div>
 
--- src/pages/AcademicsPage.jsx
+++ src/pages/AcademicsPage.jsx
@@ -4,9 +4,9 @@ import AllAcademics from "../features/academics/components/AllAcademics";
 const Academics = () => {
   return (
     <>
-      <PageTitle key={"academicPage"} title={"একাডেমিক"} />
+      <PageTitle key={"academicPage"} title={"Academic"} />
       <section className="max-w-[1144px] w-[95%] mx-auto mt-28">
-        <h2 className="text-2xl font-semibold mb-8 text-center" style={{color:'pink'}}>
+        <h2 className="text-2xl font-semibold mb-8 text-center">
           একাডেমিক তথ্য
         </h2>
         <AllAcademics />
