f8fd0fc41f630ca2e2ef8a675c6087130f07988b
shakibosd <syednazmusshakib94@gmail.com>
2025-04-01 06:10:29 +0600

    feat(authority.jsx): mx-auto and block added

--- src/features/home/components/Authority.jsx
+++ src/features/home/components/Authority.jsx
@@ -3,8 +3,8 @@ import avaterImage from '/avater.png';
 
 const Authority = ({ name, title }) => {
   return (
-    <div className="p-4 rounded-lg shadow-2xl transition-all duration-300 space-y-2">
-      <img src={avaterImage} alt={`${name}'s avatar`} className="w-48 h-48 rounded-full mx-auto mb-4" />
+    <div className="p-4 rounded-lg shadow-2xl transition-all duration-300 space-y-2 block mx-auto">
+      <img src={avaterImage} alt={`${name}'s avatar`} className="w-48 h-48 rounded-full block mx-auto mb-4" />
       <h3 className="text-lg font-bold">{name}</h3>
       <h5 className="text-md">{title}</h5>
     </div>
